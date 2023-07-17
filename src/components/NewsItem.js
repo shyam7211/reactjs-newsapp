3e8e5a0a58bec0fffe93b0bc29908bc164eb685f
Shyam Sunder Prajapat <shyamsunderprajapat.nita@gmail.com>
2023-07-17 18:03:59 +0530

    Converted NewsItem.js component to Funtion based.

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,26 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 import defaultImage from './defaultImage.jpeg'
 
-export class NewsItem extends Component {
-    render() {
-        let { title, description, imageUrl, newsUrl, publishedAt, author, source } = this.props;
+const NewsItem = (props) => {
         return (
             <div>
                 <div className="card">
                     <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'50%', zIndex: 1}}>
-                        {source}
+                        {props.source}
                     </span>
-                    <img src={imageUrl?(imageUrl === ""?defaultImage:imageUrl):defaultImage} className="card-img-top" alt="..." />
+                    <img src={props.imageUrl?(props.imageUrl === ""?defaultImage:props.imageUrl):defaultImage} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h6 className="card-title">{title}...</h6>
-                        <p className="card-text">{description}...</p>
-                        <p className="card-text"><small className="text-body-secondary">By {!author ? "Unknown" : author} at {new Date(publishedAt).toGMTString()} </small></p>
-                        <a href={newsUrl} target='_blank' rel="noreferrer" className="btn btn-primary">Details</a>
+                        <h6 className="card-title">{props.title}...</h6>
+                        <p className="card-text">{props.description}...</p>
+                        <p className="card-text"><small className="text-body-secondary">By {!props.author ? "Unknown" : props.author} at {new Date(props.publishedAt).toGMTString()} </small></p>
+                        <a href={props.newsUrl} target='_blank' rel="noreferrer" className="btn btn-primary">Details</a>
                     </div>
                 </div>
             </div>
         )
-    }
 }
 
 export default NewsItem
